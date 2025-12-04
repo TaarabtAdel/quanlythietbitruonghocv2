b19f8cf823838267cafb925f83e56cdd9dc03a55
TaarabtAdel <taarabtadel32@gmail.com>
2025-12-04 20:39:11 +0700

    Fix $borrow_fake_items[$tiet]

--- resources/views/teacher/borrows/includes/borrow-item-show.blade.php
+++ resources/views/teacher/borrows/includes/borrow-item-show.blade.php
@@ -88,13 +88,15 @@
                                     </tr>
                                 </thead>
                                 <tbody class="tiet_fake_devices">
-                                    @foreach($borrow_fake_items[$tiet] as $key => $borrow_fake_item)
-                                    <tr class="fake_device_item">
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>{{ $borrow_fake_item->device_name }}</td>
-                                        <td width="100px">{{ $borrow_fake_item->quantity }}</td>
-                                    </tr>
-                                    @endforeach
+                                    @if( !empty($borrow_fake_items[$tiet]) )
+                                        @foreach($borrow_fake_items[$tiet] as $key => $borrow_fake_item)
+                                        <tr class="fake_device_item">
+                                            <td>{{ $key + 1 }}</td>
+                                            <td>{{ $borrow_fake_item->device_name }}</td>
+                                            <td width="100px">{{ $borrow_fake_item->quantity }}</td>
+                                        </tr>
+                                        @endforeach
+                                    @endif
                                 </tbody>
                             </table>
                         </div>
